bdd29eb94a14c5029c60cde30714cd56adde6b7d
Christina Silitonga <91734751+ChristinaSilitonga@users.noreply.github.com>
2021-12-14 22:11:15 +0100

    Edit jv

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
